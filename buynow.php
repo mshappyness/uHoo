ddd531cd804429d2811afc1655ce94aa0a5429c6
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-05-17 15:54:51 +0800

    add buynow page

---
+++ buynow.php
@@ -0,0 +1,78 @@
+<?php
+include('header.php');
+?>
+
+<body class="n">
+
+	 <!-- Banner with Navigation -->
+	<?php include('nav.php'); ?>
+
+	<div class="section first green" style="background: #f9f9f9 !important">
+		<div class="content valign-wrapper row">
+				<div class="col l6 ">
+					left
+				</div>
+				<div class="col l6">
+					right
+				</div>
+		</div>
+	</div>
+
+	<div class="row">
+	    <div class="col s12 buypage-tabs" style="background: #f9f9f9">
+	      <ul class="tabs" style="background: #f9f9f9; border-bottom: 1px solid #a0a1a1">
+	        <li class="tab col s2"><a class="active" href="#details">Details</a></li>
+	        <li class="tab col s2"><a href="#specifications">Specifications</a></li>
+	        <li class="tab col s1"><a href="#size">Size</a></li>
+	        <li class="tab col s2"><a href="#requirements">Requirements</a></li>
+	        <li class="tab col s1"><a href="#reviews">Reviews</a></li>
+	        <li class="tab col s2"><a href="#inthebox">In the box</a></li>
+	        <li class="tab col s2"><a href="#deliveryinfo">Delivery Info</a></li>
+	      </ul>
+	    </div>
+	    <div id="details" class="col s12">
+	    	<ul>
+	    		<li>Keep an eye on your air quality everywhere, every time.</li>
+	    		<li> Share your device and air quality data with family or friends</li>
+	    		<li>Receive notifications and alerts when the air quality is bad</li>
+	    		<li>Customize when you want to be alerted or notified about the air quality</li>
+	    		<li>Record your respiratory conditions digitally</li>
+	    		<li>See current and historical dat by the hour, day, and month</li>
+	    	</ul>
+	    </div>
+	    <div id="specifications" class="col s12">Test 2</div>
+	    <div id="size" class="col s12">Test 3</div>
+	    <div id="requirements" class="col s12">Test 4</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12">
+			<p>9 sensors can detect the following: </p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12">
+			<p> uHoo can work with these smart devices and applications:</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12">
+			<p>Business</p>
+			<p>Reduce costs, increase productivity and grow revenues by investing in the well-being of your business and properties.
+				Invest in the future with uHoo.
+
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col s12">
+			contact form
+		</div>
+	</div>
+
+
+<?php
+include('footer.php');
+?>
